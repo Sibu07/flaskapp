69a81e78fc960be14dd817a021c2361ad5572788
sibu07 <sarbes60@gmail.com>
2023-10-11 08:12:00 +0530

    add mongodb zm in app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from routes.tgbot_routes import tgbot_bp
 from routes.mongodb_routes import mongodb_bp
 from routes.mongodb_update_routes import mongodb_update_bp
 from routes.allroutes_routes import allroutes_bp
+from routes.mongodbzm_routes import mongodbzm_bp
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ app.register_blueprint(tgbot_bp)
 app.register_blueprint(mongodb_bp)
 app.register_blueprint(mongodb_update_bp)
 app.register_blueprint(allroutes_bp)
+app.register_blueprint(mongodbzm_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
